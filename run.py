5933fc6005e2b1becb596efd451dcd775d046f48
Hao Tran <haovn577@gmail.com>
2019-05-02 20:43:49 +0700

    ...

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import cv2
 import docdetect
 import numpy as np
+import sys
 import urllib.request
 url='http://172.16.50.57:8080/shot.jpg'
 
@@ -8,12 +9,17 @@ url='http://172.16.50.57:8080/shot.jpg'
 # video = cv2.VideoCapture(video_path)
 cv2.startWindowThread()
 cv2.namedWindow('output')
+
+model = sys.argv[2]
+# print (model)
+edge_detection = cv2.ximgproc.createStructuredEdgeDetection(model)
+
 while True:
     imgResp=urllib.request.urlopen(url)
     imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
     frame=cv2.imdecode(imgNp,-1)
     # ret, frame = video.read()
-    rects = docdetect.process(frame)
+    rects = docdetect.process(frame, edge_detection)
     frame = docdetect.draw(rects, frame)
     cv2.imshow('output', frame)
     cv2.waitKey(1)
